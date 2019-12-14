29dd2bf5e524cd8191603296bf3235449ecbca19
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-14 05:16:51 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -8,22 +8,28 @@ import five from '../assets/stars/5-stars.png'
 
 const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 
+
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
-  }
+    
+    if (this.props.IMDBRating === null) {
+      return <h4>{'No rating found'}</h4>
+    } else {
+      return <img src={imgMapper[this.props.IMDBRating]} />
+    }
+}  
 
   render() {
-     let gnr = this.props.genres;
     return (
+
       <div className="card-back">
-        <h3 className="title">{this.props.title}</h3>
+       <h3 className="title">{this.props.title}</h3>
         <span />
         {this.generateRatingElement()}
         <span />
-        <h5 className="genres">{typeof gnr ==='string' ? gnr : gnr.join(', ')}</h5>
+        <h5 className="genres">{this.props.genres.join(", ")}</h5>
       </div>
-      )
+    )
   }
 }
